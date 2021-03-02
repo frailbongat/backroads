1cdc818054cd2aedb721c786ea1ee55e0567db47
Frail Bongat <frail.bongat@gmail.com>
2021-03-03 01:21:57 +0800

    Added bootstrap dependency

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-import { Children } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const layout = ({ children }) => {
   return (
