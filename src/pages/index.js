d444b1b1c93217d9e1b533b26e0c2c533e266f5f
Frail Bongat <frail.bongat@gmail.com>
2021-03-05 23:32:41 +0800

    Added react icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FaBeer } from "react-icons/fa"
 import Layout from "../components/Layout/Layout"
 
 const home = () => {
   return (
     <Layout>
+      <FaBeer />
       Hello world!
       <Link to="/blog">Go to blog page</Link>
     </Layout>
