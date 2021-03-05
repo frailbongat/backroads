5f76ddf25b772a1c74fa8fdd88a86d2faad7b00d
Frail Bongat <frail.bongat@gmail.com>
2021-03-05 23:19:44 +0800

    Added global styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,8 @@
 import React from "react"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./Layout.css"
 
 const layout = ({ children }) => {
   return (
